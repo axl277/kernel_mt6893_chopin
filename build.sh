44765eb0fa428277a836c47d709c4ef7f848970d
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-09-03 14:54:23 +0700

    Modify build script for ReSukiSU integration
    Updated device name and integrated ReSukiSU setup.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Compile script for Hydrogen kernel
+# Compile script for Hydrogen kernel (ReSukiSU Integrated)
 # Brought to you by rio004
 #
 
@@ -9,7 +9,7 @@ SECONDS=0
 DATE=$(date '+%Y%m%d-%H%M')
 
 # Device
-DEVICE="${1:-agate}"
+DEVICE="${1:-chopin}"
 DEFCONFIG="${DEVICE}_defconfig"
 ZIPNAME="HydrogenKernel-${DEVICE}-${DATE}.zip"
 
@@ -33,13 +33,18 @@ for arg in "$@"; do
     case $arg in
         -c) CLEAN_BUILD=true ;;
         -ksu) INCLUDE_KSU=true
-             ZIPNAME="HydrogenKernel-KSU-${DEVICE}-${DATE}.zip"
-             ;;
+              ZIPNAME="HydrogenKernel-ReSukiSU-${DEVICE}-${DATE}.zip"
+              ;;
     esac
 done
 
 [ "$CLEAN_BUILD" = true ] && rm -rf out
-[ "$INCLUDE_KSU" = true ] && echo "Save your stuff!!" && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+
+# ReSukisu
+if [ "$INCLUDE_KSU" = true ]; then
+    echo "Integrating ReSukiSU..."
+    curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s main
+fi
 
 # Compilation process
 mkdir -p out
@@ -48,15 +53,12 @@ make O=out ARCH=arm64 $DEFCONFIG
 echo -e "\nStarting compilation...\n"
 if make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz; then
     echo -e "\nKernel compiled successfully! Zipping up...\n"
-    git clone -q --depth=1 https://github.com/rio004/AnyKernel3 AnyKernel3
+    git clone -q --depth=1 https://github.com/axl277/AnyKernel3 AnyKernel3
     cp out/arch/arm64/boot/Image.gz AnyKernel3
     rm -rf *zip out/arch/arm64/boot
     (cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder)
     rm -rf AnyKernel3
-    if [ "$INCLUDE_KSU" = true ]; then
-        git restore drivers/{Makefile,Kconfig}
-        rm -rf KernelSU drivers/kernelsu
-    fi
+
     echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
     echo "Zip: $ZIPNAME"
 else
