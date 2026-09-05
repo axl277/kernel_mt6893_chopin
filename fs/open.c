7b4853dd65e7bf6d42dfd5ddd3ced4536a905549
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-09-05 10:24:51 +0700

    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -354,7 +354,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return error;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
+#ifdef CONFIG_KSU_SUSFS
 __attribute__((hot))
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
 				int *mode, int *flags);
@@ -375,7 +375,7 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
+#ifdef CONFIG_KSU_SUSFS
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
 
